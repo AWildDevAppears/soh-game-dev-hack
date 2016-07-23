f11e8ed354cdf43b74ed1ea55b5353ca2319b24c
Bob Whitelock <bob.whitelock1@gmail.com>
2016-07-23 12:21:56 +0100

    Fix bug/feature where all bricks became balls

--- js/classes/bricks.js
+++ js/classes/bricks.js
@@ -1,5 +1,6 @@
-var Brick = function (parent, pos, name, asset) {
-    var brick = parent.create(pos, name, asset);
+var Brick = function (parent, posx, posy,  name, asset) {
+    console.log(name) // eslint-disable-line no-console
+    var brick = parent.create(posx, posy, name, asset);
     brick.body.bounce.set(1);
     brick.body.immovable = true;
 
