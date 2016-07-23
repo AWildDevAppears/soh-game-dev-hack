290c51ff43427fe5c73c540a97acd47176386310
woJburgess <jburgess@whiteoctober.co.uk>
2016-07-23 11:53:34 +0100

    Add in gulp file to serve project

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp      = require('gulp');
+
+var rename    = require('gulp-rename');
+var connect   = require('gulp-connect');
+var watch     = require('gulp-watch');
+
+
+var paths = {
+  templates:  ['./**/*.html']
+};
+
+gulp.task('watch', function () {
+  gulp.watch(paths.sass, ['sass']);
+});
+
+gulp.task('server', function() {
+  connect.server({
+    livereload: true,
+    port: 1337,
+    host: "localhost",
+    root: '.'
+  });
+});
+
+gulp.task('livereload', function() {
+  gulp.src(['css/*.css', 'js/**/*.js', '**/*.html'])
+    .pipe(watch(['css/*.css', 'js/**/*.js', '**/*.html']))
+    .pipe(connect.reload());
+});
+
+gulp.task('default', ['server', 'livereload', 'watch']);
