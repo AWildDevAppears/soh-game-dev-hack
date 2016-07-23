ad874654651ba8b7cb3ba92f6f26269e08b7698c
Bob Whitelock <bob.whitelock1@gmail.com>
2016-07-23 12:26:59 +0100

    Remove console.log

--- js/classes/bricks.js
+++ js/classes/bricks.js
@@ -1,5 +1,4 @@
 var Brick = function (parent, posx, posy,  name, asset) {
-    console.log(name) // eslint-disable-line no-console
     var brick = parent.create(posx, posy, name, asset);
     brick.body.bounce.set(1);
     brick.body.immovable = true;
